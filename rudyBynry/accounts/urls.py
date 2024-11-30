f0d1a87a64c70b09fe6153cf19ec7c8d55c7f5c1
Rudradev-Arya <rudradev93@gmail.com>
2024-11-30 10:10:00 +0530

    bug fixes and added home page

--- rudyBynry/accounts/urls.py
+++ rudyBynry/accounts/urls.py
@@ -2,8 +2,10 @@
 
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
+    # path('login/', views.user_login, name='login'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
 ]
